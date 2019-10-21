a3968e9b33e8e541a69a4d235c7df214b5356307
Hai. Bui The <thehai.bui@gmail.com>
2019-10-21 16:41:13 +0700

    fix define icon in assets

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -3,7 +3,7 @@ class Assets {
   static const String appIcon = "assets/launcher/ic_launcher.png";
   static const String appIconForeground = "assets/launcher/ic_foreground.png";
   static const String appIconBackground = "assets/launcher/ic_background.png";
-  static const String googleButton = "assets/google.png'";
+  static const String googleButton = "assets/google.png";
   static const String social = 'assets/social.png';
   static const String placeholder = 'assets/placeholder.png';
 }
